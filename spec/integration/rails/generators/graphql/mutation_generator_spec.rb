93ae018f883a6b4a87e8f82630ee6fcca376eafd
Camilo Forero <camilo.forero3@googlemail.com>
2021-08-07 12:36:33 -0500

    Mutation generator and spec refactor
    The mutation generator has been refactored to inherit from `Rails::Generators::NamedBase` instead of `Rails::Generators::Base`
    The code for scaffolding a mutation base type and root type has been moved into it's own generator, and everything refactored to use it
    The mutation generator spec now works without the need to generate a whole Rails app and GraphQL installation on each test case

--- spec/integration/rails/generators/graphql/mutation_generator_spec.rb
+++ spec/integration/rails/generators/graphql/mutation_generator_spec.rb
@@ -1,21 +1,11 @@
 # frozen_string_literal: true
 require "spec_helper"
 require "generators/graphql/mutation_generator"
-require "generators/graphql/install_generator"
+require "generators/graphql/install/mutation_root_generator"
 
 class GraphQLGeneratorsMutationGeneratorTest < BaseGeneratorTest
   tests Graphql::Generators::MutationGenerator
-
-  destination File.expand_path("../../../tmp/dummy", File.dirname(__FILE__))
-
-  def setup(directory = "app/graphql")
-    prepare_destination
-    FileUtils.cd(File.expand_path("../../../tmp", File.dirname(__FILE__))) do
-      `rails new dummy --skip-active-record --skip-test-unit --skip-spring --skip-bundle --skip-webpack-install`
-    end
-
-    Graphql::Generators::InstallGenerator.start(["--directory", directory], { destination_root: destination_root })
-  end
+  setup :prepare_destination
 
   UPDATE_NAME_MUTATION = <<-RUBY
 module Mutations
@@ -48,22 +38,51 @@ module Types
 end
 RUBY
 
-  test "it generates an empty resolver by name" do
-    setup
+  NAMESPACED_UPDATE_NAME_MUTATION = <<-RUBY
+module Mutations
+  class Names::UpdateName < BaseMutation
+    # TODO: define return fields
+    # field :post, Types::PostType, null: false
+
+    # TODO: define arguments
+    # argument :name, String, required: true
+
+    # TODO: define resolve method
+    # def resolve(name:)
+    #   { post: ... }
+    # end
+  end
+end
+RUBY
+
+  NAMESPACED_EXPECTED_MUTATION_TYPE = <<-RUBY
+module Types
+  class MutationType < Types::BaseObject
+    field :update_name, mutation: Mutations::Names::UpdateName
+    # TODO: remove me
+    field :test_field, String, null: false,
+      description: "An example field added by the generator"
+    def test_field
+      "Hello World"
+    end
+  end
+end
+RUBY
+
+  test "it generates an empty resolver by name and inserts the field into the MutationType" do
     run_generator(["UpdateName"])
     assert_file "app/graphql/mutations/update_name.rb", UPDATE_NAME_MUTATION
-  end
-
-  test "it inserts the field into the MutationType" do
-    setup
-    run_generator(["UpdateName"])
     assert_file "app/graphql/types/mutation_type.rb", EXPECTED_MUTATION_TYPE
   end
 
-  test "it allows for user-specified directory" do
-    setup "app/mydirectory"
-    run_generator(["UpdateName", "--directory", "app/mydirectory"])
+  test "it generates and inserts a namespaced resolver" do
+    run_generator(["names/update_name"])
+    assert_file "app/graphql/mutations/names/update_name.rb", NAMESPACED_UPDATE_NAME_MUTATION
+    assert_file "app/graphql/types/mutation_type.rb", NAMESPACED_EXPECTED_MUTATION_TYPE
+  end
 
+  test "it allows for user-specified directory" do
+    run_generator(["UpdateName", "--directory", "app/mydirectory"])
     assert_file "app/mydirectory/mutations/update_name.rb", UPDATE_NAME_MUTATION
   end
 end
